294380b0a55303c0da6a249a7760abbcdb07563c
AriathGonzalez <84883360+AriathGonzalez@users.noreply.github.com>
2024-04-06 17:12:52 -0600

    Update part2controller.py

--- pox/part2controller.py
+++ pox/part2controller.py
@@ -1,14 +1,32 @@
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 
+# Define constants for protocol and ether types
+IPV4 = 0x0800
+ICMP_PROTO = 1  # ICMP protocol number
+ARP_ETHERTYPE = 0X0806  # ARP ether type
+
 log = core.getLogger()
+
 
 class Firewall (object):
   """
   A Firewall object is created for each switch that connects.
   A Connection object for that switch is passed to the __init__ function.
+
+  | src ip   | dst ip   | protocol | action |
+  |----------|----------|----------|--------|
+  | any ipv4 | any ipv4 | icmp     | accept |
+  | any      | any      | arp      | accept |
+  | any ipv4 | any ipv4 | ---      | drop   |
   """
-  def __init__ (self, connection):
+  def __init__(self, connection):
+    """
+     Initializes the firewall for a switch connection.
+
+     Args:
+         connection: The OpenFlow connection to the switch.
+    """
     # Keep track of the connection to the switch so that we can
     # send it messages!
     self.connection = connection
@@ -16,23 +34,71 @@ class Firewall (object):
     # This binds our PacketIn event listener
     connection.addListeners(self)
 
-    #add switch rules here
+    # Add switch rules here
+    self._install_icmp_rule()
+    self._install_arp_rule()
+    self._install_drop_rule()
+    # TODO: Ask about this flooding stuff...
 
-  def _handle_PacketIn (self, event):
+  def _install_icmp_rule(self):
+    """
+     Installs a rule to accept ICMP traffic.
+     | any ipv4 | any ipv4 | icmp | accept |
+    """
+    # Create an OpenFlow match object
+    icmp_match = of.ofp_match()
+    icmp_match.dl_type = IPV4  # Setting Ether type / length
+    icmp_match.nw_proto = ICMP_PROTO  # Setting IP protocol
+    # Create an OpenFlow action to accept the icmp traffic
+    icmp_accept_action = of.ofp_action_output(port=of.OFPP_NORMAL)
+    # Create an OpenFlow flow_mod message to install the rule
+    icmp_flow_mod = of.ofp_flow_mod()
+    icmp_flow_mod.match = icmp_match
+    icmp_flow_mod.actions.append(icmp_accept_action)
+    # Install the icmp_flow_mod message to the switch
+    self.connection.send(icmp_flow_mod)
+
+  def _install_arp_rule(self):
+    """
+     Installs a rule to accept ARP traffic.
+     | any | any | arp | accept |
+    """
+    arp_match = of.ofp_match()
+    arp_match.dl_type = ARP_ETHERTYPE
+    arp_accept_action = of.ofp_action_output(port=of.OFPP_NORMAL)
+    arp_flow_mod = of.ofp_flow_mod()
+    arp_flow_mod.match = arp_match
+    arp_flow_mod.actions.append(arp_accept_action)
+    # Install the arp_flow_mod message to the switch
+    self.connection.send(arp_flow_mod)
+
+  def _install_drop_rule(self):
+    """
+     Installs a default rule to drop all other IPv4 traffic.
+     | any ipv4 | any ipv4 | --- | drop |
+    """
+    # To drop every other traffic, simply do nothing
+    pass
+
+  def _handle_PacketIn(self, event):
     """
     Packets not handled by the router rules will be
     forwarded to this method to be handled by the controller
+
+    Args:
+            event: The PacketIn event triggered by the switch.
     """
 
-    packet = event.parsed # This is the parsed packet data.
+    packet = event.parsed  # This is the parsed packet data.
     if not packet.parsed:
       log.warning("Ignoring incomplete packet")
       return
 
-    packet_in = event.ofp # The actual ofp_packet_in message.
-    print ("Unhandled packet :" + str(packet.dump()))
+    packet_in = event.ofp  # The actual ofp_packet_in message.
+    print("Unhandled packet :" + str(packet.dump()))
 
-def launch ():
+
+def launch():
   """
   Starts the component
   """
